f7c8aff565c42adc88e962c38bbcc4ab43db583b
seaiin <namefinal@gmail.com>
2017-04-27 11:19:20 +0700

    add week7/box

--- ProbSolv/week7/box.cpp
+++ ProbSolv/week7/box.cpp
@@ -6,27 +6,7 @@ int n, m;
 bool isMove = false;
 
 void canMove(char map[][30], int i, int j) {
-	cout << i << j << endl;
 
-	if (isMove) {
-		return;
-	}
-
-	if (i >= 0 && j >= 0 && i + 1 < n && j + 1 < m) {
-		canMove(map, i + 1, j);
-		canMove(map, i - 1, j);
-		canMove(map, i, j + 1);
-		canMove(map, i, j - 1);
-	}
-
-	if (map[i][j] != '.' || map[i][j + 1] != '.' ||
-		map[i + 1][j] != '.' || map[i + 1][j + 1] != '.') {
-		return;
-	}
-
-	if (i + 2 == n) {
-		isMove = true;
-	}
 }
 
 int main() {
@@ -41,7 +21,5 @@ int main() {
 	}
 
 	canMove(map, 0, 0);
-
-	cout << isMove << endl;
 
 }
