51d04a17eb835b93bf2907acc0134af7be6d8d0e
seaiin <namefinal@gmail.com>
2017-05-02 16:41:42 +0700

    week10/inc start/end point

--- ProbSolv/week10/inc.cpp
+++ ProbSolv/week10/inc.cpp
@@ -1,43 +1,46 @@
 #include <iostream>
 
+#define MAX_N 10000
+
 using namespace std;
 
-int n, maximum, maxSeq = 0, point;
-int seq[1000], range[1000];
+int n, point, longest = 0, maxLenght;
+int seq[MAX_N], lenght[MAX_N], maxPoint[MAX_N];
+
+void printSeq(int n) {
+	if (maxPoint[n] != -1) {
+		printSeq(maxPoint[n]);
+	}
+}
 
 int main() {
-
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
 		cin >> seq[i];
+		lenght[i] = 1;
+		maxPoint[i] = -1;
 	}
 
 	for (int i = 0; i < n; i++) {
-		maximum = seq[i];
-		range[i] = 1;
-		for (int j = i; j < n; j++) {
-			if (seq[j] > maximum) {
-				maximum = seq[j];
-				range[i]++;
+		maxLenght = 0;
+		for (int j = 0; j < i; j++) {
+			if (seq[i] > seq[j] && lenght[j] > maxLenght) {
+				maxLenght = lenght[j];
+				maxPoint[i] = j;
+			}
+			lenght[i] = maxLenght + 1;
+			if (lenght[i] >= longest) {
+				longest = lenght[i];
+				point = i;
 			}
 		}
-		if (range[i] >= maxSeq) {
-			maxSeq = range[i];
-			point = i;
-		}
 	}
 
-	maximum = -1000001;
+	cout << longest << endl;
 
-	cout << range[point] << endl;
-
-	for (int i = point; i < n; i++) {
-		if (seq[i] > maximum) {
-			cout << seq[i] << " ";
-			maximum = seq[i];
-		}
+	for (int i = 0; i < n; i++) {
+		cout << i << " " << maxPoint[i] << endl;
 	}
 
-	cout << endl;
 }
