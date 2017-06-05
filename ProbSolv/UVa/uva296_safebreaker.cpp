18e9d2a2c21b79202806d6e1cca6a0ef7351c782
seaiin <namefinal@gmail.com>
2017-06-05 18:48:35 +0700

    add UVa/safebreaker

---
+++ ProbSolv/UVa/uva296_safebreaker.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, q;
+	char code[4], clue[3];
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> q;
+
+		for (int j = 0; j < q; j++) {
+
+		}
+	}
+}
